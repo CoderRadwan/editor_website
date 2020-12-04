436772f7044be6257162e5e8b92298ccc5fd896d
coderradwan <rad.zaam@gmail.com>
2020-12-04 22:29:27 +0800

    v00

---
+++ app.js
@@ -0,0 +1,15 @@
+let codes = document.getElementById("codes");
+let remove = document.getElementById("remove");
+let play = document.getElementById("play");
+let result = document.getElementById("result");
+
+play.onclick = ()=>{
+    result.innerHTML = codes.value ;
+    localStorage.setItem("R" , codes.value);
+};
+remove.onclick =()=>{
+    result.innerHTML = ""; 
+}
+onload = () =>{
+    codes.value =  localStorage.getItem("R");
+}
